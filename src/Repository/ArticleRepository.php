e4ad8c8f035310823a8827163823792878132d7b
manueljun17 <manueljun17@gmail.com>
2019-06-09 13:06:58 +0800

    All about Entity Repositories

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -19,22 +19,13 @@ class ArticleRepository extends ServiceEntityRepository
         parent::__construct($registry, Article::class);
     }
 
-    // /**
-    //  * @return Article[] Returns an array of Article objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @return Article[]
+     */
+    public function findAllPublishedOrderedByNewest()
     {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
     }
-    */
+    
 
     /*
     public function findOneBySomeField($value): ?Article
